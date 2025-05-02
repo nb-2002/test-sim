160e84128d0ec00aaf49c2a50c1bf01ee6cadb71
nb-2002 <nbbbyambaa@gmail.com>
2025-05-03 03:09:53 +0900

    Cleaned indexPage.vue

---
+++ src/components/canvas.ts
@@ -0,0 +1,212 @@
+export class Canvas{
+    public canvas: HTMLCanvasElement | null = null;
+    public context: CanvasRenderingContext2D | null = null;
+    public scale:number = 1;
+    public originX:number = 0;
+    public originY:number = 0;
+    public isDragging:boolean = false;
+    public lastX:number = 0;
+    public lastY:number = 0;
+    public isMoveButtonClicked:boolean = false;
+    public isDrawButtonClicked:boolean = false;
+    public isTrackPointDragged:boolean = false;
+    public arrayTrackPoints: number[][][] = [[[]]];
+    public atpi: number = 0; // arrayTrackPointsIndex
+
+    constructor(canvas:HTMLCanvasElement|null){
+        this.canvas = canvas;
+    }
+
+    public resizeCanvas(){
+        if (this.canvas) {
+            this.canvas.width = window.innerWidth;
+            this.canvas.height = window.innerHeight;
+            this.draw();
+        }
+    }
+
+    public onWheel(event: WheelEvent) {
+        event.preventDefault();
+        const zoom = event.deltaY < 0 ? 1.1 : 0.9;
+        this.scale*= zoom;
+        const mouseX = event.clientX;
+        const mouseY = event.clientY;
+        this.originX= mouseX - (mouseX - this.originX) * zoom;
+        this.originY= mouseY - (mouseY - this.originY) * zoom;
+        this.draw();
+    }
+
+    public onMouseDown(event: MouseEvent){
+        this.isDragging= true;
+        this.lastX= event.clientX;
+        this.lastY= event.clientY;
+        if(this.isDrawButtonClicked){
+            const tmpX = (this.lastX- this.originX) / this.scale, tmpY = (this.lastY- this.originY) / this.scale;
+            if(this.atpi== 0){
+                if(this.arrayTrackPoints.length == 0){
+                    this.arrayTrackPoints.push([[tmpX, tmpY]]);
+                }
+                else{
+                    this.arrayTrackPoints[0]![0]!.push(tmpX);
+                    this.arrayTrackPoints[0]![0]!.push(tmpY);
+                }
+                this.atpi++;
+            }
+            else{
+                if(this.arrayTrackPoints[this.atpi-1]!.length <= 2) this.arrayTrackPoints[this.atpi-1]!.push([tmpX, tmpY]);
+                this.arrayTrackPoints.push([[tmpX, tmpY]]);
+                this.atpi++;
+            }
+            this.draw();
+        }
+        // console.log(this.arrayTrackPoints, this.atpi);
+    }
+
+    public onMouseMove(event: MouseEvent){
+        if (this.isDragging && this.isMoveButtonClicked) {
+            const dx = event.clientX - this.lastX;
+            const dy = event.clientY - this.lastY;
+            this.originX+= dx;
+            this.originY+= dy;
+            this.lastX= event.clientX;
+            this.lastY= event.clientY;
+            this.draw();
+        }
+        if (this.isDragging && this.isDrawButtonClicked) {
+            this.isTrackPointDragged= true;
+            this.lastX= event.clientX;
+            this.lastY= event.clientY;
+            this.draw();
+        }
+    }
+
+    public onMouseUp(){
+        this.isDragging= false;
+        if(this.isTrackPointDragged){
+            this.arrayTrackPoints[this.atpi-1]!.push([(this.lastX- this.originX) / this.scale, (this.lastY- this.originY) / this.scale]);
+        }
+        this.isTrackPointDragged= false;
+    }
+
+    public draw(){
+        if (this.canvas && (this.context= this.canvas.getContext('2d'))) {
+            this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            this.context.save();
+            this.context.translate(this.originX, this.originY);
+            this.context.scale(this.scale, this.scale);
+            this.drawGrid();
+            this.drawAxis();
+            this.drawTrack();
+            this.context.restore();
+            // console.log(this.originX, this.originY, this.scale);
+        }
+    }
+
+    public drawGrid(){
+        if (this.context) {
+            const step = 50*this.scale;
+            this.context.strokeStyle = '#ccc';
+            this.context.lineWidth = (this.scale>5)?1:(this.scale<0.2)?4:2
+            for (let x = (Math.floor(-this.originX/step)*step - step/2)/ this.scale; x < (this.canvas!.width -this.originX) / this.scale; x += step/this.scale) {
+                this.context.beginPath();
+                this.context.moveTo(x, -this.originY/this.scale);
+                this.context.lineTo(x, (this.canvas!.height -this.originY)/this.scale);
+                this.context.stroke();
+            }
+            for (let y = Math.floor(-this.originY/step ) * step / this.scale; y < (this.canvas!.height -this.originY) / this.scale; y += step/this.scale) {
+                this.context.beginPath();
+                this.context.moveTo(-this.originX/this.scale, y);
+                this.context.lineTo((this.canvas!.width-this.originX)/this.scale, y);
+                this.context.stroke();
+            }
+            // for (let x = (Math.floor(-this.originX/ step) * step) / this.scale; x < (this.canvas!.width-this.originX) / this.scale; x += step) {
+            //   this.context.beginPath();
+            //   this.context.moveTo(x, -this.originY/ this.scale);
+            //   this.context.lineTo(x, (this.canvas!.height-this.originY) / this.scale);
+            //   this.context.stroke();
+            // }
+            // for (let y = (Math.floor(-this.originY/ step) * step) / this.scale; y < (this.canvas!.height -this.originY) / this.scale; y += step) {
+            //   this.context.beginPath();
+            //   this.context.moveTo(-this.originX/ this.scale, y);
+            //   this.context.lineTo((this.canvas!.width-this.originX) / this.scale, y);
+            //   this.context.stroke();
+            // }
+        }
+    }
+
+    public drawAxis(){
+        if (this.context) { 
+            this.context.strokeStyle = '#a0a0a0';
+            this.context.lineWidth = 3;
+            this.context.beginPath();
+            this.context.moveTo(Math.floor(this.canvas!.width/50)/2*50, -this.originY/this.scale);
+            this.context.lineTo(Math.floor(this.canvas!.width/50)/2*50, (this.canvas!.height -this.originY)/this.scale);
+            this.context.moveTo(-this.originX/this.scale, Math.floor(this.canvas!.height/50)/2*50);
+            this.context.lineTo((this.canvas!.width-this.originX)/this.scale, Math.floor(this.canvas!.height/50)/2*50);
+            this.context.stroke();
+        }
+    }
+
+    public drawTrack(){
+        if(this.context){
+            for(const [ind, atp] of this.arrayTrackPoints.entries()){
+                if(this.isTrackPointDragged&& ind == this.atpi-1){
+                    this.context.beginPath();
+                    this.context.arc((this.lastX- this.originX) / this.scale, (this.lastY- this.originY) / this.scale, 5, 0, Math.PI * 2);
+                    this.context.fillStyle = '#1111aa';
+                    this.context.fill();
+                    continue;
+                }
+                if(atp.length == 2){
+                    this.context.strokeStyle = '#111111';
+                    this.context.lineWidth = 50
+                    this.context.beginPath();
+                    this.context.moveTo(atp[0]![0]!, atp[0]![1]!);
+                    this.context.lineTo(atp[1]![0]!, atp[1]![1]!);
+                    this.context.stroke();
+                }
+                else if(atp.length == 3){
+                    this.context.strokeStyle = '#111111';
+                    this.context.lineWidth = 50
+                    this.context.beginPath();
+                    this.context.moveTo(atp[0]![0]!, atp[0]![1]!);
+                    this.context.quadraticCurveTo(atp[1]![0]!, atp[1]![1]!, atp[2]![0]!, atp[2]![1]!);
+                    this.context.stroke();
+                }
+                if(ind > 0 && ind < this.atpi-1){
+                    this.context.beginPath();
+                    this.context.arc(atp[0]![0]!, atp[0]![1]! , 25, 0, Math.PI * 2);
+                    this.context.fillStyle = '#111111';
+                    this.context.fill();
+                }
+            }
+        }
+    }
+
+    public centerButton(){
+        this.originX= 0;
+        this.originY= 0;
+        this.scale= 1;
+        this.draw();
+    }
+
+    public moveButtonClick(){
+        this.isDrawButtonClicked= false;
+        this.isMoveButtonClicked= !this.isMoveButtonClicked;
+    }
+
+    public drawButtonClick(){
+        this.isMoveButtonClicked= false;
+        this.isDrawButtonClicked= !this.isDrawButtonClicked;
+    }
+
+    public undoButton(){
+        if(this.atpi> 0){
+        console.log(this.atpi, this.arrayTrackPoints);
+        this.arrayTrackPoints.pop();
+        this.atpi-= 1;
+        if(this.atpi> 0) this.arrayTrackPoints[this.atpi-1]!.pop();
+        this.draw();
+        }
+    }
+}
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,234 +1,34 @@
 <template>
   <div class="canvas-container" >
     <div class="onCanvas">
-      <button class="button" @click="centerButton"> center </button>   
-      <button class="button buttons" :class="{button_active: isMoveButtonClicked}" @click="moveButtonClick"> move </button>   
-      <button class="button buttons" :class="{button_active: isDrawButtonClicked}" @click="drawButtonClick"> draw </button>   
-      <button class="button buttons" @click="undoButton"> undo </button>   
+      <button class="button" @click="canvas!.centerButton"> center </button>   
+      <button class="button buttons" :class="{button_active: canvas?.isMoveButtonClicked}" @click="canvas!.moveButtonClick"> move </button>   
+      <button class="button buttons" :class="{button_active: canvas?.isDrawButtonClicked}" @click="canvas!.drawButtonClick"> draw </button>   
+      <button class="button buttons" @click="canvas!.undoButton"> undo </button>   
     </div>
-      <canvas @wheel="onWheel" 
-       @mousedown="onMouseDown" 
-       @mouseup="onMouseUp" 
-       @mouseleave="onMouseUp" 
-       @mousemove="onMouseMove"
-       ref="canvas"></canvas>
+      <canvas @wheel="canvas!.onWheel" 
+       @mousedown="canvas!.onMouseDown" 
+       @mouseup="canvas!.onMouseUp" 
+       @mouseleave="canvas!.onMouseUp" 
+       @mousemove="canvas!.onMouseMove"
+       ref="refcanvas"></canvas>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
+import { Canvas } from 'components/canvas';
 
-const canvas = ref<HTMLCanvasElement | null>(null);
-const context = ref<CanvasRenderingContext2D | null>(null);
-const scale = ref(1.0);
-const originX = ref(0.0);
-const originY = ref(0.0);
-const isDragging = ref(false);
-const lastX = ref(0.0);
-const lastY = ref(0.0);
-const isMoveButtonClicked = ref(false);
-const isDrawButtonClicked = ref(false);
-const isTrackPointDragged = ref(false);
-const arrayTrackPoints = ref<number[][][]>([[[]]]);
-const atpi = ref(0); // arrayTrackPointsIndex
-
-const resizeCanvas = () => {
-  if (canvas.value) {
-    canvas.value.width = window.innerWidth;
-    canvas.value.height = window.innerHeight;
-    draw();
-  }
-};
-
-function onWheel(event: WheelEvent) {
-  event.preventDefault();
-  const zoom = event.deltaY < 0 ? 1.1 : 0.9;
-  scale.value *= zoom;
-  const mouseX = event.clientX;
-  const mouseY = event.clientY;
-  originX.value = mouseX - (mouseX - originX.value) * zoom;
-  originY.value = mouseY - (mouseY - originY.value) * zoom;
-  draw();
-}
-
-const onMouseDown = (event: MouseEvent) => {
-  isDragging.value = true;
-  lastX.value = event.clientX;
-  lastY.value = event.clientY;
-  if(isDrawButtonClicked.value){
-    const tmpX = (lastX.value - originX.value) / scale.value, tmpY = (lastY.value - originY.value) / scale.value;
-    if(atpi.value == 0){
-      if(arrayTrackPoints.value.length == 0){
-        arrayTrackPoints.value.push([[tmpX, tmpY]]);
-      }
-      else{
-        arrayTrackPoints.value[0]![0]!.push(tmpX);
-        arrayTrackPoints.value[0]![0]!.push(tmpY);
-      }
-      atpi.value ++;
-    }
-    else{
-      if(arrayTrackPoints.value[atpi.value-1]!.length <= 2) arrayTrackPoints.value[atpi.value-1]!.push([tmpX, tmpY]);
-      arrayTrackPoints.value.push([[tmpX, tmpY]]);
-      atpi.value ++;
-    }
-    draw();
-  }
-  // console.log(arrayTrackPoints.value, atpi.value);
-};
-
-const onMouseMove = (event: MouseEvent) => {
-  if (isDragging.value && isMoveButtonClicked.value) {
-    const dx = event.clientX - lastX.value;
-    const dy = event.clientY - lastY.value;
-    originX.value += dx;
-    originY.value += dy;
-    lastX.value = event.clientX;
-    lastY.value = event.clientY;
-    draw();
-  }
-  if (isDragging.value && isDrawButtonClicked.value) {
-    isTrackPointDragged.value = true;
-    lastX.value = event.clientX;
-    lastY.value = event.clientY;
-    draw();
-  }
-};
-
-const onMouseUp = () => {
-  isDragging.value = false;
-  if(isTrackPointDragged.value){
-      arrayTrackPoints.value[atpi.value-1]!.push([(lastX.value - originX.value) / scale.value, (lastY.value - originY.value) / scale.value]);
-  }
-  isTrackPointDragged.value = false;
-};
-
-const draw = () => {
-  if (canvas.value && (context.value = canvas.value.getContext('2d'))) {
-    context.value.clearRect(0, 0, canvas.value.width, canvas.value.height);
-    context.value.save();
-    context.value.translate(originX.value, originY.value);
-    context.value.scale(scale.value, scale.value);
-    drawGrid();
-    drawAxis();
-    drawTrack();
-    context.value.restore();
-    // console.log(originX.value, originY.value, scale.value);
-  }
-};
-
-const drawGrid = () => {
-  if (context.value) {
-    const step = 50*scale.value;
-    context.value.strokeStyle = '#ccc';
-    context.value.lineWidth = (scale.value>5)?1:(scale.value<0.2)?4:2
-    for (let x = (Math.floor(-originX.value/step)*step - step/2)/ scale.value ; x < (canvas.value!.width -originX.value) / scale.value; x += step/scale.value) {
-      context.value.beginPath();
-      context.value.moveTo(x, -originY.value/scale.value);
-      context.value.lineTo(x, (canvas.value!.height -originY.value)/scale.value);
-      context.value.stroke();
-    }
-    for (let y = Math.floor(-originY.value /step ) * step / scale.value; y < (canvas.value!.height -originY.value) / scale.value; y += step/scale.value) {
-      context.value.beginPath();
-      context.value.moveTo(-originX.value/scale.value, y);
-      context.value.lineTo((canvas.value!.width-originX.value)/scale.value, y);
-      context.value.stroke();
-    }
-    // for (let x = (Math.floor(-originX.value / step) * step) / scale.value; x < (canvas.value!.width-originX.value) / scale.value; x += step) {
-    //   context.value.beginPath();
-    //   context.value.moveTo(x, -originY.value / scale.value);
-    //   context.value.lineTo(x, (canvas.value!.height-originY.value) / scale.value);
-    //   context.value.stroke();
-    // }
-    // for (let y = (Math.floor(-originY.value / step) * step) / scale.value; y < (canvas.value!.height -originY.value) / scale.value; y += step) {
-    //   context.value.beginPath();
-    //   context.value.moveTo(-originX.value / scale.value, y);
-    //   context.value.lineTo((canvas.value!.width-originX.value) / scale.value, y);
-    //   context.value.stroke();
-    // }
-  }
-};
-
-const drawAxis = () => {
-  if (context.value) { 
-    context.value.strokeStyle = '#a0a0a0';
-    context.value.lineWidth = 3;
-    context.value.beginPath();
-    context.value.moveTo(Math.floor(canvas.value!.width/50)/2*50, -originY.value/scale.value);
-    context.value.lineTo(Math.floor(canvas.value!.width/50)/2*50, (canvas.value!.height -originY.value)/scale.value);
-    context.value.moveTo(-originX.value/scale.value, Math.floor(canvas.value!.height/50)/2*50);
-    context.value.lineTo((canvas.value!.width-originX.value)/scale.value, Math.floor(canvas.value!.height/50)/2*50);
-    context.value.stroke();
-  }
-};
-
-const drawTrack = () => {
-  if(context.value){
-    for(const [ind, atp] of arrayTrackPoints.value.entries()){
-      if(isTrackPointDragged.value && ind == atpi.value - 1){
-        context.value.beginPath();
-        context.value.arc((lastX.value - originX.value) / scale.value, (lastY.value - originY.value) / scale.value, 5, 0, Math.PI * 2);
-        context.value.fillStyle = '#1111aa';
-        context.value.fill();
-        continue;
-      }
-      if(atp.length == 2){
-        context.value.strokeStyle = '#111111';
-        context.value.lineWidth = 50
-        context.value.beginPath();
-        context.value.moveTo(atp[0]![0]!, atp[0]![1]!);
-        context.value.lineTo(atp[1]![0]!, atp[1]![1]!);
-        context.value.stroke();
-      }
-      else if(atp.length == 3){
-        context.value.strokeStyle = '#111111';
-        context.value.lineWidth = 50
-        context.value.beginPath();
-        context.value.moveTo(atp[0]![0]!, atp[0]![1]!);
-        context.value.quadraticCurveTo(atp[1]![0]!, atp[1]![1]!, atp[2]![0]!, atp[2]![1]!);
-        context.value.stroke();
-      }
-      if(ind > 0 && ind < atpi.value-1){
-        context.value.beginPath();
-        context.value.arc(atp[0]![0]!, atp[0]![1]! , 25, 0, Math.PI * 2);
-        context.value.fillStyle = '#111111';
-        context.value.fill();
-      }
-    }
-  }
-}
+const refcanvas = ref<HTMLCanvasElement | null>(null);
+const canvas = ref<Canvas| null>(null);
 
 onMounted(() => {
-  resizeCanvas();
-  window.addEventListener('resize', resizeCanvas);
-});
-
-const centerButton = () => {
-  originX.value = 0;
-  originY.value = 0;
-  scale.value = 1;
-  draw();
-}
-
-const moveButtonClick = () => {
-  isDrawButtonClicked.value = false;
-  isMoveButtonClicked.value = !isMoveButtonClicked.value;
-}
-
-const drawButtonClick = () => {
-  isMoveButtonClicked.value = false;
-  isDrawButtonClicked.value = !isDrawButtonClicked.value;
-}
-
-const undoButton = () => {
-  if(atpi.value > 0){
-    console.log(atpi.value, arrayTrackPoints.value);
-    arrayTrackPoints.value.pop();
-    atpi.value -= 1;
-    if(atpi.value > 0) arrayTrackPoints.value[atpi.value-1]!.pop();
-    draw();
+  if(refcanvas.value){
+    canvas.value = new Canvas(refcanvas.value);
   }
-}
+  canvas.value!.resizeCanvas();
+  window.addEventListener('resize', canvas.value!.resizeCanvas);
+});
 </script>
 
 <style scoped>
